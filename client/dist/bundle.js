dfc09407651c6b7f25a7935a4fefa6cd234ac475
Rose <rkoron007@gmail.com>
2020-03-10 11:34:18 -0700

    Reroll keys

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -353,9 +353,8 @@ var fetchItems = function fetchItems() {
 /***/ (function(module, exports) {
 
 module.exports = {
-  mongoURL: "mongodb://stripe-demo:1password@dbh11.mlab.com:27117/stripe-demo",
-  stripeTestKey: "pk_test_FXWm83ADvIWPVIlBHKonpw5500Hje1TX0Z",
-  secretTestKey: "sk_test_Jiml2aW8pm6n6tkPWaPcOiXq00K13rc3a2"
+  mongoURL: "mongodb://demoUser12:wHA8@Faz@fHt37!@dbh11.mlab.com:27117/stripe-demo",
+  stripeTestKey: "pk_test_FYfoqd73eGQxfaOJnaHS6lsl00hy9cM4EL"
 };
 
 /***/ }),
--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURL: "mongodb://stripe-demo:1password@dbh11.mlab.com:27117/stripe-demo",
-  stripeTestKey: "pk_test_FXWm83ADvIWPVIlBHKonpw5500Hje1TX0Z",
-  secretTestKey: "sk_test_Jiml2aW8pm6n6tkPWaPcOiXq00K13rc3a2"
-};
